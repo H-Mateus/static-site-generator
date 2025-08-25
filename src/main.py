f1e52cdbae9b875c25e7106a40b7045fdc087d67
Mateus Harrington <Bernardo-HarringtonG@cardiff.ac.uk>
2025-08-25 22:48:18 +0100

    Add funciton to clear public dir and copy static contents

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+from copy_static import copy_files_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 
 def main():
-    example = TextNode("This is some text", TextType.LINK, "https://google.com")
-    print(example)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 
 if __name__ == "__main__":
